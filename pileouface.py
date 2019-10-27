84bd5617607735ababe5534691eaf4bb12118dca
Letar <55959686+TheLetar@users.noreply.github.com>
2019-10-27 20:15:10 +0100

    Update pileouface.py

--- pileouface.py
+++ pileouface.py
@@ -3,8 +3,8 @@ import random
 def PileouFace():
     r = random.randint(0, 1) 
     if r == 1:
-        return print("Vous êtes tombé sur Pile !")
+        print("Vous êtes tombé sur Pile !")
     if r == 0:
-        return print("Vous êtes tombé sur Face !")
+        print("Vous êtes tombé sur Face !")
 
 PileouFace()
